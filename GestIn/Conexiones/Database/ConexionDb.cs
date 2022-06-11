a040e5325511d1b545a34404e447e7c6662c12e2
Rafa <rodriguezviaurafael@gmail.com>
2022-06-10 21:39:58 -0300

    CREATED conexion con base de datos

---
+++ GestIn/Conexiones/Database/ConexionDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GestIn.Conexiones.Database
+{
+    public abstract class ConexionDb
+    {
+        private readonly string connectionString;
+        protected ConexionDb()
+        {
+            connectionString = "" +
+                "Server=LAPTOP-E6PCVT2U\\SQLEXPRESS;" +
+                "DataBase = DbGestin; " +
+                "integrated security= true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
