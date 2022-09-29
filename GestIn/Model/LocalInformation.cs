63ecd3f2504eea94848ac8440c9b2ea80eb41c3e
Rafa <rodriguezviaurafael@gmail.com>
2022-09-29 18:44:30 -0300

    Fix

---
+++ GestIn/Model/LocalInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestIn.Model
+{
+    internal static class LocalInformation
+    {
+        const string connectionString = "";
+        public static string getConnectionString() {
+            return connectionString;
+        }
+    }
+}
